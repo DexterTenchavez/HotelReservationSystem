18c69b24545af4da90d71394d4b2a547b6d1e486
Dexter Tenchavez <dextertenchavez@gmail.com>
2025-11-11 11:21:07 +0800

    edit and update

--- HotelReservationSystem/Models/RegisterViewModel.cs
+++ HotelReservationSystem/Models/RegisterViewModel.cs
@@ -13,6 +13,18 @@ namespace HotelReservationSystem.Models
         public string FullName { get; set; } = null!;
 
         [Required]
+        [Phone]
+        public string PhoneNumber { get; set; } = null!;
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required]
+       
+        public string Address { get; set; } = null!;
+
+        [Required]
         [DataType(DataType.Password)]
         public string Password { get; set; } = null!;
 
